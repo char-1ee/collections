0cd0bc228d040c9ccf6d35c66523c58823fa7bac
Li Xingjian <xingjianli59@gmail.com>
2022-06-21 15:28:02 +0800

    Add sleep sort

--- cpp/range_check.c
+++ cpp/range_check.c
@@ -1,16 +1,25 @@
-// range checker in c and efficient methods.
+// range checker in c and efficient methods and all equivalent.
 
 #include <stdio.h>
 
 int main() {
     // normal range checker
-    if (x >= minx && x <= maxx) { printf("range check\n"); }
+    if (x >= minx && x <= maxx) 
+    {
+        printf("range check\n"); 
+    }
 
-    // level 2 rnage checker
-    if (((x - minx) | (maxx - x)) >= 0) {printf("range check\n");}
+    // level 2 range checker
+    if (((x - minx) | (maxx - x)) >= 0) 
+    {
+        printf("range check\n");
+    }
 
     // level 3 range checker
-    if ((unsigned) (x - minx) <= (unsigned) (maxx - minx)) {printf("range check\n");}
+    if ((unsigned) (x - minx) <= (unsigned) (maxx - minx)) 
+    {
+        printf("range check\n");
+    }
 
     return 0;
 }
