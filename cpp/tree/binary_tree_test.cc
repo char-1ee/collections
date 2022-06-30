e38281f90a5234f2f38f8a6a38eddf58b746c278
char-1ee <xingjianli59@gmail.com>
2022-07-01 07:26:43 +0800

    Add cpp tree

---
+++ cpp/tree/binary_tree_test.cc
@@ -0,0 +1,21 @@
+/* Test Driver for BinaryTree class (TestBinaryTree.cpp) */
+#include <iostream>
+#include "binary_tree.h"
+using namespace std;
+ 
+int main() {
+   BinaryTree<int> t1;
+   t1.insert(6);
+   t1.insert(10);
+   t1.insert(5);
+   t1.insert(15);
+   t1.insert(7);
+   t1.insert(4);
+   t1.insert(9);
+ 
+   t1.preOrderTraversal();
+   t1.inOrderTraversal();
+   t1.postOrderTraversal();
+   cout << t1;
+   t1.breadthFirstTraversal();
+}
